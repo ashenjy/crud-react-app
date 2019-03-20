eb07fe60dcb0386f33d7da89d1f079d91b8fa93e
ashen <jayasinghe.ashen@gmail.com>
2019-03-20 18:15:40 +0530

    added new components, react-router, bootstrap

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <h2>React CRUD Tutorial</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
