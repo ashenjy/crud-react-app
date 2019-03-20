4f88dcb938db823cd8fe2ef8dd22ed5deb1d2e54
ashen <jayasinghe.ashen@gmail.com>
2019-03-20 22:26:07 +0530

    Create bootsrap form

--- src/components/edit.component.js
+++ src/components/edit.component.js
@@ -4,7 +4,7 @@ class Edit extends Component {
     render() {
         return (
             <div>
-                
+              <p>Welcome to Edit Component!!</p>  
             </div>
         );
     }
